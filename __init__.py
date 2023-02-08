a48ce329197ae419e79a23c51009e4c53791ae7e
Felix <fedecraft3312@gmail.com>
2023-02-08 18:20:20 -0300

    Going to create a toml parser

--- __init__.py
+++ __init__.py
@@ -1,4 +1,22 @@
 import argparse
+
+from src.submodules import process_utils
 from src.libs.packages import Packages
 
+parser = argparse.ArgumentParser(description="Python app manager")
+parser.add_argument("--path", type=str,
+                    help="Application path.")
+parser.add_argument("--start", action="store_true",
+                    help="Start the application by the given path.")
+
+# Parse args
+args = parser.parse_args()
+
+# Check if the path was given
+app_path = args.path
+if not app_path:
+    raise Exception("No path given, use --path [APP PATH] to give the path "
+                    "to an application.")
+
 pkgs = Packages()
+
